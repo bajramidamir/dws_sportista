11d0728ca2de3ac0b3bb9ed95f37a6c60bfa3b75
Saida Šarić <saidasaric11@gmail.com>
2024-05-19 10:47:23 +0200

    Frontend forme, i homepage za guesta za sada

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App';
+import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import ManagerReqPage from './pages/ManagerReqPage';
+
+
+
+const router = createBrowserRouter([
+  {path: '/',
+    element: <HomePage/>
+  },
+  {path: '/login',
+    element: <LoginPage/>
+  },
+  {path: '/signup',
+    element: <SignupPage/>
+  },
+  {path: '/postanimenadzer',
+  element: <ManagerReqPage/>
+
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+<React.StrictMode>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
