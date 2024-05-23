4c246eb019c84a64aea07878fc9314d06f9edc93
IbrahimLazovic <155297901+IbrahimLazovic@users.noreply.github.com>
2024-05-23 19:57:54 +0200

    Update UserPage.jsx

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,1 +1,13 @@
+import React, {useState} from "react";
 
+import App from "../components/UserPanel/App";
+
+function AdminPage(){
+    return(
+        <div className="text-gray-600 font-mono">
+            <div className="grid md:grid-cols-3">
+                <App />
+            </div>
+        </div>
+    )
+}
