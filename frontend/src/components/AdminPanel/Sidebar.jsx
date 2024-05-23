460aec870c972b65794c531cc1a49b5cdb3c34f5
IbrahimLazovic <155297901+IbrahimLazovic@users.noreply.github.com>
2024-05-23 19:30:06 +0200

    Update Sidebar.jsx

--- frontend/src/components/AdminPanel/Sidebar.jsx
+++ frontend/src/components/AdminPanel/Sidebar.jsx
@@ -1,1 +1,67 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
+
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+    return (
+      <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+          <div className='sidebar-title'>
+              <div className='sidebar-brand'>
+                  SPORTSMAN
+              </div>
+              <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+          </div>
+  
+          <ul className='sidebar-list'>
+          
+          <li className='sidebar-list-item'>
+            <a href="#glavna-ploca"> 
+                   Glavna ploča
+                </a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door" viewBox="-5 0 20 10">
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
+                </svg>
+                
+            </li>
+
+              <li className='sidebar-list-item'>
+                  <a href="">
+                       Tereni <BsFillArchiveFill className='icon'/>
+                  </a>
+              </li>
+              <li className='sidebar-list-item'>
+                <a href="">
+                      Termini
+                  </a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="16" fill="currentColor" class="bi bi-clock" viewBox="-5 0 20 16">
+  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+</svg>
+                  
+              </li>
+              <li className='sidebar-list-item'>
+                  <a href="">
+                      Igrači <BsPeopleFill className='icon'/> 
+                  </a>
+              </li>
+              <li className='sidebar-list-item'>
+                <a href="#menadzeri">
+                    Menadžeri <BsListCheck className='icon'/> 
+                </a>
+            </li>
+              <li className='sidebar-list-item'>
+                  <a href="">
+                     Postavke <BsFillGearFill className='icon'/> 
+                  </a>
+              </li>
+          </ul>
+      </aside>
+    )
+  }
+
+export default Sidebar
