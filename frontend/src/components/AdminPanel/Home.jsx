e60d1cb5ebc1a4813e65b1d9fbe4cf5e024a8f58
IbrahimLazovic <155297901+IbrahimLazovic@users.noreply.github.com>
2024-05-23 19:29:15 +0200

    Update Home.jsx

--- frontend/src/components/AdminPanel/Home.jsx
+++ frontend/src/components/AdminPanel/Home.jsx
@@ -1,1 +1,27 @@
+import React from 'react'
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
 
+/*funkcionalnosti lampice i slike profila moramo napraviti*/
+
+function Header({OpenSidebar}) {
+  return (
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
+        </div>
+        <div className='header-left'>
+            <BsSearch  className='icon'/>
+            <input class="datatable-input" placeholder="Search..." type="search" title="Search">
+            </input>
+        </div>
+        <div className='header-right'>
+            
+            <BsPersonCircle className='icon'/>
+        </div>
+    </header>
+  )
+}
+
+export default Header
